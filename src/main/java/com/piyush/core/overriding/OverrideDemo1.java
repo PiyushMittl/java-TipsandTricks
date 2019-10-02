c749547bb1c6ac6b06cca3332d8b9198ab8b151f
piyushmittl <piyush.mittal@rediffmail.com>
2019-10-02 12:16:30 +0530

    override class updated

--- src/main/java/com/piyush/core/overriding/OverrideDemo1.java
+++ src/main/java/com/piyush/core/overriding/OverrideDemo1.java
@@ -7,17 +7,21 @@ class Parent {
 	}
 }
 class Child1 extends Parent {
-	static String s="Child";
+	static String s="Child1";
 	public void print() {
 		System.out.println(s);
 	}
 }
 
 class Child2 extends Parent {
-	static String s="Child";
-//	public void print() {
-//		System.out.println(s);
-//	}
+	static String s="Child2";
+}
+
+class Child3 extends Parent {
+	static String s="Child3";
+	public void print() {
+		System.out.println(s);
+	}
 }
 
 public class OverrideDemo1{
@@ -40,26 +44,36 @@ public class OverrideDemo1{
 		
 //Question: data hiding works when we do not override the method
 /*Tip 2*/		
-		Parent p1=new Child2();
+		System.out.println("tip2");
+		Parent p1=new Child2();//no overridden method in child
 		p1.print();
 		//here no hiding would happen because there is no method overriding 
 		//reason: because if you do data hiding method might get corrupted because you might declare Object s=new ***() not string. which will cause a disaster.
+		
+		p1=new Child3();//overriding method
+		p1.print();
+		//here hiding would happen because child is having overridden method
 
 //Question: can we down-cast parent reference having parent object. can we down-cast parent reference having child object. 		
 /*Tip 3 - storing parent reference to child*/		
-		System.out.println();
-		
-		
+		System.out.println("---Tip3---");
 		Parent p3=new Parent();
-		Child1 c3=(Child1)p3;//run time exception
+		//Parent p3=args[0]; //it can be anything
+		//Child1 c3=(Child1)p3;//run time exception
 		
-		Parent p4=new Child2();
+		//c3.print();   ----> Break here, because c3 contains any object of Parent class and do not about Child object and its methods
+		
+		Parent p4=new Child1();
 		Child1 c4=(Child1)p4;//works well
 
 //Question: since we can not override data members, then how can we access child data members from parent reference variable containing child object.		
 /*Tip 4 - accessing child data member using parent reference by down-casting */
 		Parent p5=new Child1();
 		System.out.println(((Child1)p5).s);//works well
+		
+		Parent p6=new Parent();
+		System.out.println(((Child1)p6).s);//works well
+		
 
 //Question: is it possible to access parent data member by using child reference containing child object
 /*Tip 5 - accessing parent data member using child reference by up-casting */
