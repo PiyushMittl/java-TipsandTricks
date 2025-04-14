911a4d79db90f4fd00db2fa9daab57d3cc81288c
Piyush Mittal <mr.piyushmittal@rediffmail.com>
2025-04-14 17:39:28 +0000

    adding sample queue class

---
+++ src/main/java/poc/queue/QueueExample.java
@@ -0,0 +1,19 @@
+package main.java.poc.queue;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> queue=new PriorityQueue<>();
+        queue.add(10);
+        queue.add(50);
+        queue.add(20);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        
+    }
+    
+}
