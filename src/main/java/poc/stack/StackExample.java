27c307943c2d91df97e4bce6ddd820388cef54ca
Piyush Mittal <mr.piyushmittal@rediffmail.com>
2025-04-14 17:23:33 +0000

    adding sample stack class

---
+++ src/main/java/poc/stack/StackExample.java
@@ -0,0 +1,20 @@
+package main.java.poc.stack;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+     
+        Stack<Integer> stack=new Stack<>();
+        stack.add(10);
+        stack.add(20);
+        stack.add(30);
+
+        System.out.println("stack size="+stack.size());
+        System.out.println("Peek="+stack.peek());
+        System.out.println("stack size after Peek="+stack.size());
+        
+        System.out.println("Peek="+stack.pop());
+        System.out.println("stack size after Pop="+stack.size());        
+    }
+}
