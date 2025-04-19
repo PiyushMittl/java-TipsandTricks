7e3854a3fb650f089331f036d96c7cd9e891ebcf
Piyush Mittal <mr.piyushmittal@rediffmail.com>
2025-04-19 10:10:42 +0000

    adding next greater element sample class

---
+++ src/main/java/poc/stack/NGE.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class NGE{
+    public static void main(String[] args) {
+        int[] arr =new int[] {7,10,4,3,20,15};
+        Stack<Integer> s new Stack<>();
+
+        for(int val: arr){
+            while(s.size()>0 && s.peek()<val){
+                s.pop();
+            }
+            System.out.println(s.size()>0? s.peek():-1);
+            s.push(val);
+        }
+    }
+}
