773458503dd1d011c7b0d5f0a13907160dc2cf0d
Piyush Mittal <mr.piyushmittal@rediffmail.com>
2025-04-19 10:12:01 +0000

    fixing typo

--- src/main/java/poc/stack/NGE.java
+++ src/main/java/poc/stack/NGE.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class NGE{
     public static void main(String[] args) {
         int[] arr =new int[] {7,10,4,3,20,15};
-        Stack<Integer> s new Stack<>();
+        Stack<Integer> s = new Stack<>();
 
         for(int val: arr){
             while(s.size()>0 && s.peek()<val){
